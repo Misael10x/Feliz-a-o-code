ee6c56d3ac5ac81e2c546aba1f02ec61ffab9e8f
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-04 20:18:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+import random
+from math import sin, cos, radians
 
+class FireworkAnimation:
+    def __init__(self, canvas, x, y, color):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.color = color
+        self.particles = []
+        self.create_particles()
